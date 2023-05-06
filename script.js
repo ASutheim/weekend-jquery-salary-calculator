b0886aeaac6344787c8194637954c693c468d411
Anna Sutheim <asutheim@gmail.com>
2023-05-06 12:47:08 -0500

    handleSubmit function now works and pushes inputted data to table on the DOM

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 $(document).ready(onReady);
 
 function onReady() {
-  $(`#submit.btn`).on(`click`, handleSubmit);
+  console.log("Ready!");
+  $(`#submit-btn`).on(`click`, handleSubmit);
 }
 
 function handleSubmit(event) {
-  console.log("Submit button clicked");
   event.preventDefault();
+
+  //This code sends the submitted info to the console
+  console.log("Submit button clicked");
 
   let firstNameInput = $("#first-name-input").val();
   let lastNameInput = $("#last-name-input").val();
@@ -15,4 +18,15 @@ function handleSubmit(event) {
   let salaryInput = $("#salary-input").val();
 
   console.log(firstNameInput, lastNameInput, idInput, titleInput, salaryInput);
+
+  //This code sends the submitted info plus a delete button to the table on the DOM
+
+  $(`#employee-info-table`).append(
+    `<tr><td>${firstNameInput}</td>
+    <td>${lastNameInput}</td>
+    <td>${idInput}</td>
+    <td>${titleInput}</td>
+    <td>${salaryInput}</td>
+    <td><button class = "delete-btn">❌</button></td>`
+  );
 }
