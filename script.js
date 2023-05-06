7e43c89859dd6bddfec306cdd58db1b1d44bb2f9
Anna Sutheim <asutheim@gmail.com>
2023-05-06 10:36:10 -0500

    linked html submit button with js handle submit function (not working yet)

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 $(document).ready(onReady);
 
 function onReady() {
-  $("#submit.btn").on("click", handleSubmit);
+  $(`#submit.btn`).on(`click`, handleSubmit);
+}
+
+function handleSubmit(event) {
+  console.log("Submit button clicked");
+  event.preventDefault();
+
+  let firstNameInput = $("#first-name-input").val();
+  let lastNameInput = $("#last-name-input").val();
+  let idInput = $("#id-input").val();
+  let titleInput = $("#title-input").val();
+  let salaryInput = $("#salary-input").val();
+
+  console.log(firstNameInput, lastNameInput, idInput, titleInput, salaryInput);
 }
